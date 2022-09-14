7ebb9dce972d9353205f0049b25c58852bb8eba2
guanill <102127692+guanill@users.noreply.github.com>
2022-09-14 01:08:01 -0400

    Add files via upload

---
+++ Resources/Database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE student (
+    Numero de Estudiante INTEGER PRIMARY KEY,
+    Nombre TEXT NOT NULL,
+    Segundo Nombre TEXT,
+    Apellido Paterno TEXT NOT NULL,
+    Apellido Materno TEXT,
+    Grado INTEGER NOT NULL,
+    Edad INTEGER
+);
